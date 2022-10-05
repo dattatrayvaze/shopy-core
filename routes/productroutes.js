40bc13badef50f0ce17582badca22e4aa22e2ebe
Atitnaik23 <95529492+Atitnaik23@users.noreply.github.com>
2022-10-05 13:27:12 +0530

    Update productroutes.js

--- routes/productroutes.js
+++ routes/productroutes.js
@@ -2,11 +2,11 @@ const ProdController = require('./app/controllers/ProdController')
 const router = require('express').Router()
 
 
-router.post('/addProduct',ProdController.addProduct)
+router.post('/Product',ProdController.addProduct)
 router.get('/allProducts', ProdController.getAllProducts)
-router.get('/getFeaturedProduct', ProdController.getFeaturedProduct)
-router.get('/:id', ProdController.getOneProduct)
-router.put('/:id', ProdController.updateProduct)
-router.delete('/:id', ProdController.deleteProduct)
+router.get('/FeaturedProduct', ProdController.getFeaturedProduct)
+router.get('/product/:id', ProdController.getOneProduct)
+router.put('/product/:id', ProdController.updateProduct)
+router.delete('product/:id', ProdController.deleteProduct)
 
 module.exports = productroutes
