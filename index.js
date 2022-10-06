4152217ea0536e922a7dd68a2b504af77efacf1c
Atitnaik23 <95529492+Atitnaik23@users.noreply.github.com>
2022-10-06 10:59:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const router = express.Router();
 const webRoutes = require('./routes/web');
 const sequelize = require('./config/database');
 const errorController = require('./app/controllers/ErrorController');
-const ProductRoutes = require('./routes/productroutes');
+const productRoutes = require('./routes/productroutes');
 
 
 env.config();
